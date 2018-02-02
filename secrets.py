8ee6b8917aa6fc4383e7d4beaf86b95011601d00
miglee <mignionlee@gmail.com>
2018-02-01 22:54:15 -0500

    made the twitter-oauth file though I've no idea how it came into what it looks like

--- secrets.py
+++ secrets.py
@@ -1,2 +1,5 @@
 api_key = "	HrUiVyjnwwYOoSiXhtEFrelOP"
 api_secret = "abI5ifjkP4VBCYdntGv4wjymHyKzWh6HdPR3saVI4ff1EH5nY3"
+
+access_token = "959266620295311360-8KdzMLWcXOPl0FLktAKoikzU9LShOvh"
+access_token_secret = "WnfWts8c53woN9KljeyysqYoC1ZEmSsVB6h0fgbGfZI73"
